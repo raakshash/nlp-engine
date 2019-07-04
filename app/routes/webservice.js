8903ce6c42583134eaf77dba520fce3b68a04898
Naveen Kumar <nkr4@3ds.com>
2019-07-04 15:09:05 +0530

    [Webservice] - handled the unknow request

--- app/routes/webservice.js
+++ app/routes/webservice.js
@@ -1,34 +1,52 @@
 const express = require('express');
+const qs = require('querystring');
 const router = express.Router();
 const IntentManager = require('../controllers/intentManager');
 const ResponseManager = require('../controllers/responseManager');
 
 router.post('/getResponse/:_accessID', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    let intentID = NLP.getClassifiedData(req.body.expression).classified;
-    let resToSend = "Not trained for this";
+    if (req.method == 'POST') {
+        let body = '';
+        req.on('data', function (data) {
+            body += data;
+            if (body.length > 1e6) {
+                req.connection.destroy();
+            }
+        });
+        req.on('end', function () {
+            let POST = JSON.parse(body);
+            let intentID = "";
+            try {
+                intentID = NLP.getClassifiedData(POST.expression).classified;
+            } catch (e) {
+                console.error("Error: ", e);
+            }
+            let resToSend = "Not trained for this";
 
-    if (intentID != undefined) {
-        IntentManager.findIntent(req.params._accessID, intentID, function (err, iIntent) {
-            if (err) {
-                console.log("No intent found: " + err);
-            } else if (iIntent != undefined) {
-                ResponseManager.findAllResponses(iIntent._id, function (err, iResponses) {
-                    if (iResponses.length > 0) {
-                        let replyRandomIndex = Math.floor(Math.random() * iResponses.length);
-                        resToSend = iResponses[replyRandomIndex];
+            if (intentID != undefined) {
+                IntentManager.findIntent(req.params._accessID, intentID, function (err, iIntent) {
+                    if (err) {
+                        console.log("No intent found: " + err);
+                    } else if (iIntent != undefined) {
+                        ResponseManager.findAllResponses(iIntent._id, function (err, iResponses) {
+                            if (iResponses.length > 0) {
+                                let replyRandomIndex = Math.floor(Math.random() * iResponses.length);
+                                resToSend = iResponses[replyRandomIndex];
+                            }
+                            if (resToSend == undefined || resToSend == "") {
+                                resToSend = "Not trained for this";
+                            }
+                            res.json({ intent: iIntent, fulfillment: resToSend, expression: req.body.expression });
+                        });
+                    } else {
+                        res.json({ intent: null, fulfillment: resToSend, expression: req.body.expression });
                     }
-                    if (resToSend == undefined || resToSend == "") {
-                        resToSend = "Not trained for this";
-                    }
-                    res.json({ intent: iIntent, fulfillment: resToSend, expression: req.body.expression });
                 });
             } else {
                 res.json({ intent: null, fulfillment: resToSend, expression: req.body.expression });
             }
         });
-    } else {
-        res.json({ intent: null, fulfillment: resToSend, expression: req.body.expression });
     }
 });
 
